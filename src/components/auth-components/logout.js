e1f1d5c15f3028a9596b57592bcd0abb17286705
mischb <mischarberlin@gmail.com>
2019-01-17 11:57:31 -0500

    fixed logout

--- src/components/auth-components/logout.js
+++ src/components/auth-components/logout.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { clearAuth } from '../../actions/auth';
 import { connect } from 'react-redux';
+import { clearAuthToken } from '../../local-storage';
 import './logout.css';
 export class Logout extends Component {
-  clickHandler() {
+  logout() {
     this.props.dispatch(clearAuth());
+    clearAuthToken();
   }
   render() {
     return (
-      <button className="button logout" onClick={() => this.clickHandler()}>
+      <button className="button logout" onClick={() => this.logout()}>
         Logout
       </button>
     );
