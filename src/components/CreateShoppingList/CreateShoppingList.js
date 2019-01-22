bad075ea8059ddf66d4e671f815bd7489d3615b7
SamDaltonJr <42707637+SamDaltonJr@users.noreply.github.com>
2019-01-22 10:43:02 -0600

    Revert "Feature/store search"

--- src/components/CreateShoppingList/CreateShoppingList.js
+++ src/components/CreateShoppingList/CreateShoppingList.js
@@ -4,7 +4,6 @@ import { Redirect } from 'react-router-dom';
 
 import NavBar from '../nav-bar';
 import CreateShoppingListForm from '../CreateShoppingListForm';
-import StoreSearch from '../StoreSearch';
 
 function CreateShoppingList({ user, loggingIn }) {
   if (loggingIn) {
@@ -23,7 +22,6 @@ function CreateShoppingList({ user, loggingIn }) {
           <h1 className="CreateShoppingList-pageTitle">New shopping list</h1>
         </header>
         <CreateShoppingListForm />
-        <StoreSearch />
       </main>
     </div>
   );
--- src/components/StoreSearch/index.js
+++
@@ -1,3 +0,0 @@
-import StoreSearch from './StoreSearch';
-
-export default StoreSearch;
