08cd00a73c6f1eb0aab5c7b3425f51624b946046
mischb <mischarberlin@gmail.com>
2019-01-23 18:03:27 -0500

    aisle prompt disappears on click anywhere but aisleprompt compontent

--- src/components/AddAisle.js
+++ src/components/AddAisle.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { patchItem, removeAislePrompt } from '../actions/items';
 export class AddAisle extends Component {
+  componentDidMount() {
+    document.addEventListener('mousedown', this.onClick, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.onClick, false);
+  }
+
+  onClick = e => {
+    const { dispatch } = this.props;
+    if (this.addAisleForm.contains(e.target)) {
+      return;
+    }
+    dispatch(removeAislePrompt());
+  };
   updateAisle(e) {
     e.preventDefault();
     const { dispatch, listId, item } = this.props;
@@ -16,7 +30,10 @@ export class AddAisle extends Component {
   render() {
     const { item } = this.props;
     return (
-      <form onSubmit={e => this.updateAisle(e)}>
+      <form
+        onSubmit={e => this.updateAisle(e)}
+        ref={addAisleForm => (this.addAisleForm = addAisleForm)}
+      >
         <span>You found the {item.name}. Which aisle are you in?</span>
         <input type="text" ref={input => (this.input = input)} />
         <button>Update Aisle</button>
