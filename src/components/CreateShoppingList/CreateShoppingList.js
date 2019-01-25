8e3042447a79d273847bc150bf6bdd31ade2f045
Arun Seehra <seehra.a@gmail.com>
2019-01-25 17:44:59 -0600

    Restore CreateShoppingList.css
    Again, I think this was removed by accident during merge conflict.

--- src/components/CreateShoppingList/CreateShoppingList.js
+++ src/components/CreateShoppingList/CreateShoppingList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+import './CreateShoppingList.css';
 import CreateShoppingListForm from '../CreateShoppingListForm';
 
 function CreateShoppingList({ user, loggingIn, currentStore }) {
