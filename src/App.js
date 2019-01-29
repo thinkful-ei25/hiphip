905d265646f4c0c2c20dd9b9b768c0845900f52e
Arun Seehra <seehra.a@gmail.com>
2019-01-28 23:42:07 -0600

    Rearrange router so that the landing page is now /
    Also, / will become the default route.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import RegistrationPage from './components/auth-components/registration-page';
 import LoginPage from './components/auth-components/login-page';
 import Lists from './components/Lists/Lists.js';
 import Items from './components/Items';
-import CreateShoppingList from './components/CreateShoppingList';
 import LandingPage from './components/LandingPage';
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={LoginPage} />
-        <Route exact path="/landing" component={LandingPage} />
+        <Route exact path="/login" component={LoginPage} />
         <Route exact path="/register" component={RegistrationPage} />
         <Route exact path="/lists" component={Lists} />
-        <Route exact path="/lists/create" component={CreateShoppingList} />
         <Route exact path="/lists/:listId" component={Items} />
+        <Route path="/" component={LandingPage} />
       </Switch>
     );
   }
